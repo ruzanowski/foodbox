e6b649e02446a373bca765e175d67b73bf02cba4
Sebastian <sebastian.ruzanowski@gmail.com>
2020-08-16 13:31:49 +0200

    corrected ci

--- src/Food.Web.Host/docker-publish.sh
+++ src/Food.Web.Host/docker-publish.sh
@@ -8,4 +8,4 @@ apk add python python-dev py-pip build-base libffi-dev openssl-dev
 pip install docker-compose
 docker login -u $CI_REGISTRY_USER -p "$CI_REGISTRY_PASSWORD" $CI_REGISTRY
 docker build -t registry.gitlab.com/ruzanowski/foodbox/foodbox:$DOCKER_TAG .
-docker push registry.gitlab.com/ruzanowski/ubiquitous/productservice:$DOCKER_TAG
+docker push registry.gitlab.com/ruzanowski/foodbox/foodbox:$DOCKER_TAG
