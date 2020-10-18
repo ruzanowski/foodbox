0db05b977956eaaebf8792b3194cdf719e4a29ec
Sebastian <sebastian.ruzanowski@gmail.com>
2020-10-19 01:28:17 +0200

    Preferable time optional now

--- src/Food.Web.Host/Food.Application/Orders/Dto/DeliveryTime/CreateDeliveryTimeDto.cs
+++ src/Food.Web.Host/Food.Application/Orders/Dto/DeliveryTime/CreateDeliveryTimeDto.cs
@@ -6,7 +6,7 @@ namespace Food.Orders.Dto.DeliveryTime
     [AutoMapTo(typeof(Ordering.DeliveryTime))]
     public class CreateDeliveryTimeDto
     {
-        public DateTime PreferableTimeDelivery { get; set; }
+        public DateTime? PreferableTimeDelivery { get; set; }
         public DateTime DateTime { get; set; }
     }
 }
