974b9d746dbeb8f5794f2b90ae524d1ac200d3e1
Sebastian <sebastian.ruzanowski@gmail.com>
2020-10-19 01:36:13 +0200

    Preferable time optional now

--- src/Food.Web.Host/Food.Application/Orders/Dto/DeliveryTime/CreateDeliveryTimeDto.cs
+++ src/Food.Web.Host/Food.Application/Orders/Dto/DeliveryTime/CreateDeliveryTimeDto.cs
@@ -6,7 +6,6 @@ namespace Food.Orders.Dto.DeliveryTime
     [AutoMapTo(typeof(Ordering.DeliveryTime))]
     public class CreateDeliveryTimeDto
     {
-        public DateTime? PreferableTimeDelivery { get; set; }
         public DateTime DateTime { get; set; }
     }
 }
--- src/Food.Web.Host/Food.Application/Orders/Dto/DeliveryTime/DeliveryTimeDto.cs
+++ src/Food.Web.Host/Food.Application/Orders/Dto/DeliveryTime/DeliveryTimeDto.cs
@@ -6,7 +6,6 @@ namespace Food.Orders.Dto.DeliveryTime
     [AutoMapFrom(typeof(Ordering.DeliveryTime))]
     public class DeliveryTimeDto
     {
-        public DateTime? PreferableTimeDelivery { get; set; }
         public DateTime DateTime { get; set; }
     }
 }
